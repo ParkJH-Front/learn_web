e46972421c973ad7b69a6170a2d14d7d016560cf
ParkJH-Front <100084383+ParkJH-Front@users.noreply.github.com>
2022-06-10 17:52:35 +0900

    06.10
    JS 시작

---
+++ lv1/make/JS/app.js
@@ -0,0 +1,9 @@
+const navBtn = document.getElementsByClassName("navBtn")
+
+console.log(navBtn)
+
+function navBtnColor() {
+  console.log("wow!")
+}
+
+navBtn[0].addEventListener("mouseover", navBtnColor); 
