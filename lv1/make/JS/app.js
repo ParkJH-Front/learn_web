0756663cb202197552fdd08854d2f7af8d96e419
ParkJH-Front <100084383+ParkJH-Front@users.noreply.github.com>
2022-06-13 17:37:15 +0900

    06.13
    forEach 학습중

--- lv1/make/JS/app.js
+++ lv1/make/JS/app.js
@@ -2,8 +2,11 @@ const navBtn = document.getElementsByClassName("navBtn")
 
 console.log(navBtn)
 
+
 function navBtnColor() {
   console.log("wow!")
 }
 
-navBtn[0].addEventListener("mouseover", navBtnColor); 
+navBtn.forEach((element) => {
+  console.log(element);
+})
