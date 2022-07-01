868996c48844d1ad427e1a64af4d868c4fcc578b
ParkJH-Front <100084383+ParkJH-Front@users.noreply.github.com>
2022-07-01 17:20:01 +0900

    07.01
    진도가 안나가네 ..

--- lv3/myself/js/herotext.js
+++ lv3/myself/js/herotext.js
@@ -6,25 +6,52 @@ const worldList = [
 "San Francisco."
 ]
 const target = document.querySelector(".world_name")
-let num = 0
-let worldName = worldList[num]
-let selectWorldName = worldName.split("")
 
-function typing(typingArr){
-  if(0<typingArr.length) {
-    num = num+1
-    target.textContent += typingArr.shift()
-    console.log(typingArr)
-    console.log(num)
-    setInterval(function more(){
-      typing(selectWorldName)
-    }, 200)
+
+// Array 지정할 숫자를 하나씩 늘림.
+function nextNum(number) {
+  if(number < 4) {
+    number = number +1
+    return number
+  } else {
+    number = 0
+    return number
   }
 }
 
-function retyping(typingArr) {
-  typing(selectWorldName)
+// 글자를 하나씩 타이핑함.
+function typing(nameArr) {
+  target.classList.add("cursor")
+  if(0<nameArr.length) {
+    target.textContent += nameArr.shift()
+  }
 }
 
-setInterval(retyping, 100)
-console.log(num)
+// 글자를 하나씩 지움.
+function remove() {
+  const reName = target.innerText
+  const reNameArr = reName.split("")
+  if(0<reNameArr.length) {
+    reNameArr.pop()
+    target.textContent = reNameArr.join("")
+  } else {
+    check()
+  }
+}
+
+//타이핑 실행 조건
+function check(worldName) {
+  if(0<worldName.length) {
+    typing(worldName)
+  } else {
+    remove(worldName)
+  }
+}
+
+let number = 0
+
+setInterval(function() {
+  const worldName = worldList[number]
+  const selectName = worldName.split("")
+  check(selectName)
+}, 200)
