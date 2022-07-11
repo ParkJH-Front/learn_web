28735eb67225b37f5fe4a52f0cd2aa8fa1c02c39
ParkJH-Front <100084383+ParkJH-Front@users.noreply.github.com>
2022-07-11 15:10:33 +0900

    07.11
    작업중

--- lv3/myself/js/herotext.js
+++ lv3/myself/js/herotext.js
@@ -4,60 +4,61 @@ const worldList = [
   "Maui.",
   "London.",
   "San Francisco."
-  ]
-  const target = document.querySelector(".world_name")
-  let num = worldList.length
-  
-  // 기존의 Array를 새로운 Array로 변경 (array.map 사용을 위한 함수)
-  function createArr(oldArr) {
-    return oldArr
-  }
+]
+const target = document.querySelector(".world_name")
+let num = 0
 
-  // 전달받은 Array를 한글자씩 출력함.
-  function wriht(selectName) {
-    const rowArray = selectName.map(createArr)
-    const typing = setInterval(function aa(){
-      if(0<selectName.length) {
-        target.textContent += selectName.shift()
-      } else {
-        clearInterval(typing)
-        remove(rowArray)
-        console.log(rowArray)
-      }
-    }, 200)
-  }
-  
-  // 전달받은 Array를 한글자씩 지움
-  function remove(selectName) {
-    console.log(selectName)
-    const removing = setInterval(function bb() {
-      if(0<selectName.length) {
-        selectName.pop()
-        target.textContent = selectName.join("")
-      } else {
-        clearInterval(removing)
-        nameSelect()
-        // setTimeout(function cc(){
-        //   clearInterval(removing)
-        //   nameSelect()
-        // }, 200)
-      }
-    }, 200)
-  }
-  
-  //worldList 순차적으로 선택하여 다룸
-  function nameSelect() {
-    target.classList.add("cursor")
-    if (num < 5) {
-      const selectName = worldList[num].split("") 
-      wriht(selectName)
-  
-      num = num + 1
-      
+// 기존의 Array를 새로운 Array로 변경 (array.map 사용을 위한 함수)
+function createArr(oldArr) {
+  return oldArr
+}
+
+// writh() 실행 시 관련 이미지가 표시됨.
+function heroImg() {
+
+}
+
+// 전달받은 Array를 한글자씩 출력함.
+function writh(selectName) {
+  heroImg()
+  const rowArray = selectName.map(createArr)
+  const typing = setInterval(function aa() {
+    if (0 < selectName.length) {
+      target.textContent += selectName.shift()
     } else {
-      num = 0
+      clearInterval(typing)
+      setTimeout(function cc() {
+        remove(rowArray)
+      }, 2000)
+    }
+  }, 200)
+}
+
+// 전달받은 Array를 한글자씩 지움
+function remove(selectName) {
+  const removing = setInterval(function bb() {
+    if (0 < selectName.length) {
+      selectName.pop()
+      target.textContent = selectName.join("")
+    } else {
+      clearInterval(removing)
       nameSelect()
     }
+  }, 200)
+}
+
+//worldList 순차적으로 선택하여 다룸
+function nameSelect() {
+  target.classList.add("cursor")
+  if (num < worldList.length) {
+    console.log(num)
+    const selectName = worldList[num].split("")
+    writh(selectName)
+    num = num + 1
+  } else {
+    num = 0
+    nameSelect()
   }
-  
-  nameSelect()
+}
+
+nameSelect()
